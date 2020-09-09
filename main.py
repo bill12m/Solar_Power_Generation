5df5cd3c5793a265cc332e7d38529e9885be2952
Bill <wjm6g3@mail.umsl.edu>
2020-09-08 21:29:17 -0500

    ac and dc predict yield better apart than they do together

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ sp.call('clear', shell = True)
 
 #Import datasets for Plant 1
 eod_df = pd.read_csv('data/Plant_1_End_of_Day.csv').set_index('DATE_TIME')
-#ax = sns.lineplot(x = eod_df.index, y = 'DAILY_YIELD',data = eod_df,
-#                  hue = 'SOURCE_KEY')
+ax = sns.lineplot(x = eod_df.index, y = 'DAILY_YIELD',data = eod_df,
+                  hue = 'SOURCE_KEY')
 
 #Run Ridge Regression on each inverter's data.
 score = []
 for inverter in eod_df['SOURCE_KEY'].unique():
     eod_df_test = eod_df[eod_df['SOURCE_KEY'] == inverter]
-    X = eod_df_test[['DC_POWER', 'AC_POWER']]
+    X = eod_df_test[['AC_POWER']]
     y = eod_df_test['DAILY_YIELD']
     
     X_train, X_val, y_train, y_val = train_test_split(X, y,
                                                       test_size = 0.1, shuffle = False,
                                                       random_state = 12)
-    reg = linear_model.LinearRegression()
+    reg = linear_model.Ridge(alpha = 0.5)
     reg.fit(X_train, y_train)
     score.append(reg.score(X_val, y_val))
